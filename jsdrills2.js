e812fbea5b84f74d428b73229b27f469de52425d
Kyle Rogers <kylerogers334@gmail.com>
2017-05-08 19:00:07 +0000

    Added to infinity function

--- jsdrills2.js
+++ jsdrills2.js
@@ -2,6 +2,23 @@ function jediName(firstName, lastName){
     return `${lastName.slice(0, 3) + firstName.slice(0, 2)}`
 }
 
-console.log(jediName('Beyonce', 'Knowles'));
+// console.log(jediName('Beyonce', 'Knowles'));
 
 
+function beyond(num){
+    function posOrNeg(num){
+        if (Math.sign(num) === 1){
+            console.log('To infinity');
+        } else {
+            console.log('To negative infinity');
+        }
+    }
+    switch(num) {
+        case Infinity:
+        case -Infinity : console.log('And beyond'); break;
+        case 0 : console.log('Staying Home'); break;
+        default : posOrNeg(num); break;
+    }
+}
+
+beyond(Infinity);
