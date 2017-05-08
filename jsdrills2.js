135bc26f2e666dc20deb3878a9727d37c14647b9
Kyle Rogers <kylerogers334@gmail.com>
2017-05-08 19:43:08 +0000

    Finish rock paper scissors

--- jsdrills2.js
+++ jsdrills2.js
@@ -39,4 +39,53 @@ function decodeSentence(sentence) {
     return sentence.map(decode).join('');
 }
 
-console.log(decodeSentence("craft block argon meter bells brown croon droop"));
+//console.log(decodeSentence("craft block argon meter bells brown croon droop"));
+
+function daysInMonth(month) {
+    function printInfo(month, days) {
+        console.log(`${month} has ${days} days.`);
+    }
+    switch (month) {
+        case 'January': printInfo(month, 31); break;
+        case 'February': printInfo(month, 28.25); break;
+        case 'March': printInfo(month, 31); break;
+        case 'April': printInfo(month, 30); break;
+        case 'May': printInfo(month, 31); break;
+        case 'June': printInfo(month, 30); break;
+        case 'July': printInfo(month, 31); break;
+        case 'August': printInfo(month, 31); break;
+        case 'September': printInfo(month, 31); break;
+        case 'October': printInfo(month, 30); break;
+        case 'November': printInfo(month, 31); break;
+        case 'December': printInfo(month, 30); break;
+        default: console.log("Thats not a month.");
+    }
+}
+
+//daysInMonth('February')
+
+function RockPaperScissors(user){
+  let computer = ['rock', 'paper','scissors'][Math.floor((Math.random()*3))];
+  
+  if (computer === 'rock'){
+      switch (user) {
+          case "scissors": return "Rock beats scissors. Computer Wins.";
+          case "paper": return "Paper beats Rock. User wins.";
+          default: return "Tie";
+      }
+  } else if (computer === 'paper') {
+      switch (user) {
+          case "rock": return "Paper beats Rock. Computer wins.";
+          case "scissors": return "Scissors beats paper. User wins.";
+          default: return "Tie";
+      }
+  } else {
+      switch (user) {
+          case "paper": return "Scissors beats paper. Computer wins.";
+          case "rock": return "Rock beats scissors. User wins.";
+          default: return "Tie";
+      }
+  }
+}
+
+console.log(RockPaperScissors('paper'));
