b305afebc67e3655f3be892456399ec442496ce1
Sonja Duric <duric.sonja@gmail.com>
2017-05-08 14:34:53 -0400

    checked for incorrectly typed arguments

--- jsdrills1.js
+++ jsdrills1.js
@@ -10,6 +10,10 @@ function whoAmI(name, age){
 		console.error("Arguments not valid.");
 		return;
 	}
+	else if(typeof age !== "number" || typeof name !== "string"){
+		console.error("Arguments are not correctly typed");
+		return;
+	}
 
 	let yOB = yearOfBirth(age);
 	console.log(`My name is ${name} and I'm ${age} years old.`);
