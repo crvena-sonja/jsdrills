ae13ac999675a7adaac68605a6e9d38f5fb92060
Sonja Duric <duric.sonja@gmail.com>
2017-05-08 14:15:15 -0400

    year birth calculation added

--- jsdrills1.js
+++ jsdrills1.js
@@ -1,5 +1,7 @@
 function whoAmI(name, age){
+	let yearOfBirth = new Date().getFullYear() - age;
 	console.log(`My name is ${name} and I'm ${age} years old.`);
+	console.log(`I was born in ${yearOfBirth}`);
 }
 
 whoAmI("Sonja", 30);
