fbd42ca4c58f5af609478ef3dfb68cc4b02d0328
Sonja Duric <duric.sonja@gmail.com>
2017-05-08 14:27:51 -0400

    checked for invalid arguments

--- jsdrills1.js
+++ jsdrills1.js
@@ -1,8 +1,16 @@
 function yearOfBirth(age) {
+	if (age < 0){
+		throw new Error("Age cannot be negative!");
+	}
 	return new Date().getFullYear() - age;
 }
 
 function whoAmI(name, age){
+	if (!name || !age){
+		console.error("Arguments not valid.");
+		return;
+	}
+
 	let yOB = yearOfBirth(age);
 	console.log(`My name is ${name} and I'm ${age} years old.`);
 	console.log(`I was born in ${yOB}.`);
